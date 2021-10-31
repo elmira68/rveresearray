9eb1a6e07f7f785616ef96bbe8bb3f4271e4bade
elmira68 <irandoostelmira@yahoo.com>
2021-10-31 21:39:10 +0200

    add file

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        static void rversearray(int []arr , int start , int end)
+        {
+            int temp;
+            if (start >= end)
+            return;
+
+            temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            rversearray(arr , start+1 , end-1);
+        }
+        static void printarray(int []arr , int size)
+        {
+            for (int i = 0; i < size; i++)
+            
+               Console.Write(arr[i] + "");
+               Console.WriteLine(""); 
+            
+        }
+    
+       
+       
+        public static void Main()
+        {
+          int []arr = {5,2,3,6,9,8};
+
+          printarray(arr , 6);
+          rversearray(arr , 0 , 5);
+          
+          Console.WriteLine("reversed array is");
+          printarray(arr , 6);
+        }
+
+    }
+}
